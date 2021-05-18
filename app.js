16d702deb003514e7413e3457fd5a37cd1e98450
Mohit <mohityede755@gmail.com>
2021-05-18 14:58:31 +0530

    login page created- frontend

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { access } = require('fs/promises');
+const app= express();
+
+const path = require('path');
+
+// middlewares
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+app.use(express.static('public'));
+
+//
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('accout/login');
+})
+
+const port = 7000;
+app.listen(port,()=>{
+    console.log("server started at port 7000");
+})
